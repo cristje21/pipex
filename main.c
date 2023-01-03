143448a1653417cd56d15eeae331747713ec1a0f
Cris Van Schijndel <cvan-sch@f0r6s5.codam.nl>
2023-01-03 20:14:33 +0100

    yeah

--- main.c
+++ main.c
@@ -6,42 +6,47 @@
 /*   By: cristje <cristje@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/30 16:43:12 by cvan-sch      #+#    #+#                 */
-/*   Updated: 2023/01/02 20:44:10 by cvan-sch      ########   odam.nl         */
+/*   Updated: 2023/01/03 20:08:41 by cvan-sch      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	test_one_command(int argc, char *argv[], char *envp[])
+char	*get_path(char **command, char **argv, char **envp)
 {
-	int 	my_pipe[2];
-	int		id;
-	int		fd;
-	char	**command;
-	
-	fd = open(argv[1], O_RDONLY);
-	pipe(my_pipe);
-	id = fork();
-	if (id != 0) // parent
+	int		check;
+	char	**paths;
+	char	*s;
+	int		i;
+
+	check = 0;
+	i - 0;
+	command = split_arg(argv[1]);
+	while (paths[i])
 	{
-		close(my_pipe[0]);
-		dup2(STDOUT_FILENO, my_pipe[1]);
-	}
-	else // child
-	{
-		close(fd);
-		close(my_pipe[1]);
-		dup2(STDIN_FILENO, my_pipe[0]);
-		command = split_arg(argv[2]);
-		printf("doing the command!\n%s\n", command[1]);
-		
-		execve("/bin/grep", command, NULL);
+		s = ft_strjoin(paths[i], command[0]);
+		if (s == NULL)
+			return (NULL);
+		if (!access(s, X_OK | F_OK))
+			return (s);
+		free(s);
+		i++;
 	}
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	test_one_command(argc, argv, envp);
+	int		input;
+	char	**command;
+	char	*path;
+	char	**paths;
+
+	paths = ft_split(envp[6][5], ':');
+	if (argc != 3)
+		return (0);
+	if ((input = open(argv[1], O_RDONLY)) == -1)
+		return (1);
+	path = get_path(command, argv, envp);
 	return (0);
 }
 
